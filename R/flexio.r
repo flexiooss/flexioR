27b3a9cef73e98dec94493182faf11526473fb91
ThibaudJeannin <thibaud.jeannin@gmail.com>
2018-03-28 10:35:08 +0200

    Cleaned code

--- R/flexio.r
+++ R/flexio.r
@@ -1,32 +1,33 @@
-#' Prints Hello World
-#' @family Random stuff
-#' @export
-helloworld <- function() {print("Array index starts from 1")}
-
-#' Max range authorized by Flexio API
+#' Max pagination range authorized by Flexio API
 #' @export
 flexioPaginationLength <- 100
 
 
 #' Returns a dataset containing ressources from Flexio
-#' @param fields name of the fields you want to get in your dataset (leave it empty if you want all the fields)
 #' @param flexioURL URL of Flexio's API
 #' @param account flexio account
 #' @param ressourceName name of the flexio ressource
 #' @param auth flexio authentification token
 #' @param header additional header (you don't need to add 'range' and 'Authorization', they have been put automaticaly for you)
+#' @param fields name of the fields you want to get in your dataset (leave it empty if you want all the fields)
 #' @param verbose set it to TRUE to print the request details
 #' @family Flexio Interaction
 #' @export
 getFlexioRessource <- function(flexioURL, account, ressourceName, auth, header=NULL, fields=c() ,verbose=FALSE) {
   requestURL <- paste(flexioURL,'/',account,'/',ressourceName, sep = "", collapse = NULL)
   rangeFrom <- 0
-  dataset <- NULL
+  dataset <- NULL #Empty dataset
 
   repeat{
     range <- sprintf("%i-%i", rangeFrom, rangeFrom + flexioPaginationLength)
 
-    req <- GET(requestURL, add_headers(Authorization=auth, range=range, header))
+    if(verbose)
+    {
+      req <- GET(requestURL, add_headers(Authorization=auth, range=range, header), verbose())
+    }
+    else{
+      req <- GET(requestURL, add_headers(Authorization=auth, range=range, header))
+    }
 
     if(! req$status_code %in% c(200,206)){print(http_status(req)$message); return(NULL)}
 
@@ -34,20 +35,19 @@ getFlexioRessource <- function(flexioURL, account, ressourceName, auth, header=N
     new_dataset <- data.frame(resp, stringsAsFactors=FALSE)
     dataset <- rbind(dataset, new_dataset)
 
-    rangeFrom <- rangeFrom + flexioPaginationLength
 
+    rangeFrom <- rangeFrom + flexioPaginationLength
     if(req$status_code == 200){break}
   }
+
   if (length(fields) != 0){
     dataset <- subset(dataset, select=fields)
   }
 
-
   return(dataset)
 }
 
-#' Sends a ressource to Flexio
-#' @param fields name of the fields you want to get in your dataset (leave it empty if you want all the fields)
+#' Sends a ressource to Flexio. Adds each entry of the given dataset to the Flexio ressource
 #' @param flexioURL URL of Flexio's API
 #' @param account flexio account
 #' @param ressourceName name of the flexio ressource
@@ -63,44 +63,47 @@ postFlexioRessource <- function(flexioURL, account, ressourceName, auth, data, v
   for (entry in 1:n)
   {
     if(length(data) == 1){
-      body <- sprintf('{\"%s\" : \"%s\"}', colnames(data),data[entry,])
+      body <- sprintf('{\"%s\" : \"%s\"}', colnames(data),data[entry,]) #Manual JSON formatting
     }
     else{
       body <- toJSON(data)
     }
-    print(body)
     if(verbose){
-      req <- POST(
-        requestURL, body=body, add_headers(Authorization=auth, 'Content-type'='application/json'), verbose())
+      req <- POST(requestURL, body=body, add_headers(Authorization=auth, 'Content-type'='application/json'), verbose())
     }
     else{
-      req <- POST(
-        requestURL, body=body, add_headers(Authorization=auth, 'Content-type'='application/json'))
+      req <- POST(requestURL, body=body, add_headers(Authorization=auth, 'Content-type'='application/json'))
     }
 
-    if(! req$status_code %in% c(201)){print(http_status(req)$message); break}
+    if(! req$status_code %in% c(201)){print(http_status(req)$message); return(FALSE)}
   }
+  return(TRUE)
 }
 
 #' Returns a 1 entry dataset containing ressources reccord from Flexio
-#' @param fields name of the fields you want to get in your dataset (leave it empty if you want all the fields)
 #' @param flexioURL URL of Flexio's API
 #' @param account flexio account
 #' @param ressourceName name of the flexio ressource
 #' @param auth flexio authentification token
 #' @param reccordID : the ID of the reccord you want
+#' @param fields name of the fields you want to get in your dataset (leave it empty if you want all the fields)
 #' @param verbose set it to TRUE to print the request details
 #' @family Flexio Interaction
 #' @export
-getFlexioReccord <- function(flexioURL, account, ressourceName, reccordID, auth, fields=c(), verbose=FALSE) {
+getFlexioReccord <- function(flexioURL, account, ressourceName, auth, reccordID, fields=c(), verbose=FALSE) {
   requestURL <- paste(flexioURL,'/',account,'/',ressourceName,'/',reccordID , sep = "", collapse = NULL)
 
-  req <- GET(requestURL, add_headers(Authorization=auth))
+  if(verbose){
+    req <- GET(requestURL, add_headers(Authorization=auth), verbose())
+  }
+  else{
+    req <- GET(requestURL, add_headers(Authorization=auth))
+  }
+
   if(! req$status_code %in% c(200)){print(http_status(req)$message); return(NULL)}
 
   resp <- fromJSON(content(req, "text"))
-
-  resp=resp[-(which(sapply(resp,is.null),arr.ind=TRUE))]
+  resp=resp[-(which(sapply(resp,is.null),arr.ind=TRUE))] #Remove the NULL fields
 
   reccord <- as.data.frame(resp, check.names=FALSE)
   if (length(fields) != 0){
@@ -108,11 +111,9 @@ getFlexioReccord <- function(flexioURL, account, ressourceName, reccordID, auth,
   }
 
   return(reccord)
-
 }
 
 #' Updates a ressource reccord from Flexio
-#' @param fields name of the fields you want to get in your dataset (leave it empty if you want all the fields)
 #' @param flexioURL URL of Flexio's API
 #' @param account flexio account
 #' @param ressourceName name of the flexio ressource
@@ -142,27 +143,24 @@ putFlexioReccord <- function(flexioURL, account, ressourceName, auth, reccordID,
 #' @param verbose set it to TRUE to print the request details
 
 #' Updates a ressource reccord from Flexio
-#' @param fields name of the fields you want to get in your dataset (leave it empty if you want all the fields)
 #' @param flexioURL URL of Flexio's API
 #' @param account flexio account
 #' @param ressourceName name of the flexio ressource
 #' @param auth flexio authentification token
 #' @param reccordID : the ID of the reccord you want
 #' @param data : the data you want to use
-#' @param fields : the fields you want to update
+#' @param fields : the fields you want to update (leave it empty to update all the fields)
 #' @family Flexio Interaction
 #' @export
 patchFlexioReccord <- function(flexioURL, account, ressourceName, auth, reccordID, data, fields=c(), verbose=FALSE) {
   requestURL <- paste(flexioURL,'/',account,'/',ressourceName,'/',reccordID , sep = "", collapse = NULL)
 
   if(length(fields) > 0){
-    # data <- subset(data, select = fields) FIXME Ne fonctionne pas quand il manque un champs
+    # data <- subset(data, select = fields) FIXME PATCH doesn't work with missing fields
   }
 
   body <- toJSON(data)
   body <- substring(body,2,nchar(body)-1)
-
-  print(body)
 
   if(verbose){
     req <- PATCH(requestURL, add_headers(Authorization=auth, 'Content-type'='application/json'), body=body, verbose())
@@ -188,7 +186,12 @@ patchFlexioReccord <- function(flexioURL, account, ressourceName, auth, reccordI
 deleteFlexioReccord <- function(flexioURL, account, ressourceName, reccordID, auth, verbose=FALSE) {
   requestURL <- paste(flexioURL,'/',account,'/',ressourceName,'/',reccordID , sep = "", collapse = NULL)
 
-  req <- DELETE(requestURL, add_headers(Authorization=auth))
+  if(verbose){
+    req <- DELETE(requestURL, add_headers(Authorization=auth), verbose())
+  }
+  else{
+    req <- DELETE(requestURL, add_headers(Authorization=auth))
+  }
   if(! req$status_code %in% c(204)){print(http_status(req)$message); return(FALSE)}
   return(TRUE)
 
